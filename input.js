a2c8e356f01e9bd32a5a99ddd6144b0119c02788
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-03 19:27:16 +0000

    added the movemnt and sending from client to server

--- input.js
+++ input.js
@@ -1,16 +1,31 @@
+let connection;
+
+
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === 'w'){
+    connection.write('Move: up');
+  } else if (data === 'a'){
+    connection.write('Move: left');
+  } else if (data === 'd'){
+    connection.write('Move: right');
+  } else if (data === 's'){
+    connection.write('Move: down');
   }
+
 }
 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.handleUserInput('data')
+  stdin.on('data',function(key){
+    handleUserInput(key);
+  });
   return stdin;
 }
 
