ba96258374c2e7f283772afa01a601516fc4a27e
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-03 17:20:25 +0000

    refactored code

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  //handel data comming from the server
-  conn.on('data', (input) => {
-    console.log(input);
-  });
-
-  return conn;
-}
+// play.js
+const { connect } = require('./client');
 
 console.log('Connecting ...');
+
 connect();
