64b825456741475275af7dd179e493764686aee6
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-03 19:49:54 +0000

    clean up

--- input.js
+++ input.js
@@ -5,36 +5,11 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   } else {
-    if(KEYS.hasOwnProperty(data)){
-      console.log(KEYS[data])
-      connection.write(KEYS[data])
-
+    if(KEYS.hasOwnProperty(data)) {
+      connection.write(KEYS[data]);
     }
   }
-  
 }
-
-
-/* const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  } else if (data === 'w'){
-    connection.write('Move: up');
-  } else if (data === 'a'){
-    connection.write('Move: left');
-  } else if (data === 'd'){
-    connection.write('Move: right');
-  } else if (data === 's'){
-    connection.write('Move: down');
-  } else if (data === 'q'){
-    connection.write('Say: Beat you');
-  } else if (data === 'f'){
-    connection.write('Say: FUN :D');
-  } else if (data === 'e'){
-    connection.write('Say: LOL');
-  } 
-} */
-
 
 
 const setupInput = function(conn) {
